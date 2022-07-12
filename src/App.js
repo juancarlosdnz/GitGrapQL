fb7519808a268211fdba341e82a89434ce4ab672
Juan Carlos Git <dnzjuancarlos@gmail.com>
2022-07-12 14:57:40 +0200

    Filtered search

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import github from "./db.js";
 import { useCallback, useEffect, useState } from "react";
 import query from "./Query"
 import RepoInfo from "./RepoInfo";
+import SearchBox from "./SearchBox.js";
 function App() {
 
   let [userName, setUsername] = useState("")
   let [repoList, setRepoList] = useState(null)
   let [pageCount, setPageCount] = useState(10)
-  let [queryString, setQueryString] = useState("lab")
+  let [queryString, setQueryString] = useState("")
   let [totalCount, setTotalCount] = useState(null)
 
   const fetchData = useCallback(() => {
@@ -23,8 +24,11 @@ function App() {
       .then((data) => {
         const viewer = data.data.viewer
         const repos = data.data.search.nodes
+        const total = data.data.search.repositoryCount
+
         setUsername(viewer.name)
         setRepoList(repos)
+        setTotalCount(total)
       })
       .catch((err) => {
         console.log(err);
@@ -39,7 +43,14 @@ function App() {
     <div className="App container mt-5">
       <h1 className="text-primary"><i className="bi bi-diagram-2-fill"></i> Repos</h1>
       <p>Hey there {userName}</p>
+      <SearchBox
+        totalCount={totalCount}
+        pageCount={pageCount}
+        queryString={queryString}
+        onQueryChange={(myString) => { setQueryString(myString) }}
+        onTotalChange={(myNumber) => { setPageCount(myNumber) }}
 
+      />
       {repoList && (
         <ul className="list-group list-group-flush">
           {repoList.map((repo) => (
--- src/Query.js
+++ src/Query.js
@@ -1,26 +1,32 @@
-const githubQuery = {
-  query: `
-      {
-        viewer {
-          name
-        }
-          search(query: "user:juancarlosdnz sort:updated-desc", type: REPOSITORY, first:30) {
-            nodes {
-              ... on Repository {
-                name
-                description
-                id
-                url
-                viewerSubscription
-                licenseInfo {
-                  spdxId
-                }
-            }
+const githubQuery = (pageCount, queryString) => {
+
+  return {
+    query: `
+    {
+      viewer {
+        name
+      }
+        search(query: "${queryString}user:juancarlosdnz sort:updated-desc", type: REPOSITORY, first:30) {
+          repositoryCount
+          nodes {
+            ... on Repository {
+              name
+              description
+              id
+              url
+              viewerSubscription
+              licenseInfo {
+                spdxId
+              }
           }
         }
       }
-    `
-  ,
+    }
+  `
+    ,
+  }
+
+
 };
 
 export default githubQuery
