b87285d53c365d3da394120a192083e2028fcd98
juancarlosdnz <dnzjuancarlos@gmail.com>
2022-07-12 09:19:23 +0200

    Initial structure and basic query

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import github from "./db.js";
+import {useCallback, useEffect,useState} from "react";
+import query from "./Query"
+
+function App() {
+
+  let [userName, setUsername] = useState("")
+  const fetchData = useCallback(()=>{
+    fetch(github.baseURL, {
+      method: "POST",
+      headers: github.headers,
+      body: JSON.stringify(query),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUsername(data.data.viewer.name)
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },[])
+
+  useEffect(()=>{
+    fetchData()
+  },[fetchData])
+  
+  return (
+    <div className="App container mt-5">
+      <h1 className="text-primary"><i className="bi bi-diagram-2-fill"></i> Repos</h1>
+      <p>Hey there {userName}</p> 
+    </div>
+  );
+}
+
+export default App;
